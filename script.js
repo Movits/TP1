ed173de1b0043d6691f4045455641d801c5ad84d
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-27 22:21:25 -0300

    hice la primer parte de la funcion generar()

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const cantProdMostrar = document.querySelector("#cant-prod-mostrar");
 const cantPermitida = document.querySelector("#cant-perm");
 const prodColores = document.querySelector("#colores-prod");
+const divContenedorProdYInfor = document.querySelector("#contenedor-productos-y-info");
+
+function generar() {
+    divContenedorProdYInfor.innerHTML = ``;
+    divContenedorProdYInfor.innerHTML += `
+        <div id="contenedor-info">
+            <p>La cantidad de productos a mostrar es: ${cantProdMostrar.value}</p>
+            <p>La cantidad de productos permitidos por compra es: ${cantPermitida.value}</p>
+            <p>Los colores seleccionados a usar son: ${prodColores.value}</p>
+        </div>
+
+        <div id="contenedor-productos">
+            
+        </div>
+    `;
+}
