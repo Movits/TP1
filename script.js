38214e7da1e594206ec0b4d83d86dcb7fda9b567
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-27 21:49:06 -0300

    agregue const

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-/* Hola ;) */
+const cantProdMostrar = document.querySelector("#cant-prod-mostrar");
+const cantPermitida = document.querySelector("#cant-perm");
+const prodColores = document.querySelector("#colores-prod");
