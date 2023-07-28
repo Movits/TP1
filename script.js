f24b248ea185d4c844b1a097a9137e7bc9eb2f2d
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-28 19:12:41 -0300

    agregue funcion colores

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const prodColores = document.querySelector("#colores-prod");
 const divContenedorProdYInfo = document.querySelector("#contenedor-productos-y-info");
 
 const IMG_1 = "https://static.vecteezy.com/system/resources/previews/009/900/525/original/pizza-slice-isolated-illustration-colored-sketch-drawn-illustration-of-a-hot-slice-of-pepperoni-pizza-with-melting-cheese-food-cafe-pizzeria-logo-signboard-banner-menu-design-element-vector.jpg";
+const IMG_2 = "https://img.freepik.com/vector-gratis/flying-slice-of-pizza-cartoon-vector-illustration-concepto-comida-rapida-vector-aislado-estilo-dibujos-animados-plana_138676-1934.jpg?w=740&t=st=1690578504~exp=1690579104~hmac=7a89704ec401c90112798f1f42a5a72f48998f7da546c1ccf580a1f5a7d964ed";
+const IMG_3 = "https://img.freepik.com/vector-premium/rebanada-dibujos-animados-pizzailustracion-dibujos-animados-vectorclipart-dibujos-animados_11393-359.jpg";
+const Imgs = [IMG_1,IMG_2,IMG_3];
 
-const Imgs = [];
-const Productos = ["Peperoni", "Margarita", "Cuatro Quesos", "Napolitana"];
+const Productos = [
+    "Peperoni",
+    "Margarita",
+    "Napolitana"
+];
+
 
 function generar() {
     divContenedorProdYInfo.innerHTML = `
@@ -38,20 +45,39 @@ function prod() {
     const divContenedorProd = document.querySelector("#contenedor-productos");
     for (let i = 0; i < ProductosMostrar; i++) {
         divContenedorProd.innerHTML += `
-            <div class="productos">
-                <p>Porción de ${Productos[i]}</p>
-                <img src="${IMG_1}" alt="img">
-                <p>Seleccione pago</p>
-                <select name="" id="formas-de-pago">
-                    <option value="Efectivo">Efectivo</option>
-                    <option value="Debito">Debito</option>
-                    <option value="Credito">Credito</option>
-                </select>
-                <p>Seleccione cantidades</p>
-                <select name="" id="${cantPermitida.innerHTML}">
-                    
-                </select>
+            <div class="div-colores">
+                <div class="productos">
+                    <p>Porción de ${Productos[i]}</p>
+                    <img src="${Imgs[i]}" alt="img">
+                    <p>Seleccione pago</p>
+                    <select name="" id="formas-de-pago">
+                        <option value="Efectivo">Efectivo</option>
+                        <option value="Debito">Debito</option>
+                        <option value="Credito">Credito</option>
+                    </select>
+                    <p>Seleccione cantidades</p>
+                    <select name="" id="${cantPermitida.innerHTML}">
+                        
+                    </select>
+                </div>
             </div>
         `;
+    }
+}
+
+function colores() {
+    const divColores = document.querySelector("#div-colores");
+    switch (prodColores.value) {
+        case "Rojo-Amarillo":
+            divColores.style.backgroundcolor = black
+            break;
+        case "Verde-Violeta":
+
+            break;
+        case "Azul-Gris":
+
+            break;
+        default:
+            break;
     }
 }
